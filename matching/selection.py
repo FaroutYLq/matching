0ac131e65c7f2a05710b9ba3983912d97117b364
Lanqing Yuan <yuanlq@uchicago.edu>
2023-04-16 02:15:27 -0500

    doc string

--- matching/selection.py
+++ matching/selection.py
@@ -12,22 +12,40 @@ class Selection:
         # Load matches
         self.matches = match_object.find_matches()
 
-class MinimumResamplingRate(Selection):
+class MinimumMatchingRate(Selection):
+    """We will check how often a data event is matched to simulation. 
+    On average, one data event will be matched simu_counts/data_counts times, and we call this 
+    ratio average matching times.
+    And min_match_rate defines the data selection criteria: one data event must be matched more than 
+    min_match_rate times average matching times, so that it will be selected.
+    """
     def __init__(self, data, simu, covariates, distance='Mahalanobis', match='NearestNeighbor'):
         super().__init__(data, simu, covariates, distance, match)
         self.data = data
         self.data_counts = len(data)
         self.simu_counts = len(simu)
 
-    def select(self, min_resample_rate):
+    def select(self, min_match_rate):
+        """Select data based on minimum sample rate. We will check how often a data event is matched to
+        simulation. On average, one data event will be matched simu_counts/data_counts times, and we call this 
+        ratio average matching times.
+        And min_match_rate defines the data selection criteria: one data event must be matched more than 
+        min_match_rate times average matching times, so that it will be selected.
+
+        Args:
+            min_match_rate (float): Minimum times of average matching to pass selection.
+
+        Returns:
+            selected_data_mask (1darray of bool): Data selection mask. True means selected.
+        """
         matches = self.matches
 
         # Count how many time one data event has been matched
         matched_counts = matches['data_index'].value_counts()
         matched_counts = matched_counts.to_frame(name='counts')
 
-        # Required minimum sampling rate
-        selected_mask = matched_counts['counts']  >= min_resample_rate*self.simu_counts/self.data_counts
+        # Required minimum matching rate
+        selected_mask = matched_counts['counts']  >= min_match_rate*self.simu_counts/self.data_counts
         selected_counts = matched_counts[selected_mask]
 
         # Assumed simulation always has smaller indices
@@ -36,6 +54,3 @@ class MinimumResamplingRate(Selection):
         selected_data_mask[selected_data_idx] = True
 
         return selected_data_mask
-    
-        
-
