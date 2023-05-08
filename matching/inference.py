a74e0aad952a03b918807672e341daa1aa3ec5aa
Lanqing Yuan <yuanlq@uchicago.edu>
2023-05-08 15:55:58 -0500

    align range for covariates

--- matching/inference.py
+++ matching/inference.py
@@ -8,7 +8,7 @@ DATA_BATCH_SIZE = 2500
 SIMU_BATCH_SIZE = 15000
 
 class Inference:
-    def __init__(self, data, simu, covariates, distance='Mahalanobis', match='NearestNeighbor'):
+    def __init__(self, data, simu, covariates, distance='Mahalanobis', match='NearestNeighbor', central_fraction=0.98):
         """Init Causal Inference by getting matching result.
 
         Args:
@@ -17,6 +17,7 @@ class Inference:
             covariates (list): A list of strings, corresponding to field names of covariates in matching.
             distance (str, optional): Distance definition. Defaults to 'mahalanobis'.
             match (str, optional): Matching method definition. Defaults to 'NearestNeighbor'.
+            central_fraction (float): Central fraction to align covariates in data and simu. Default to be 0.98.
         """
         if isinstance(data, np.ndarray):
             data = pd.DataFrame(data)
@@ -25,6 +26,13 @@ class Inference:
 
         self.distance = distance
         self.match = match
+
+        # Align range of covariates to avoid artifacts
+        self.central_fraction = central_fraction
+        data_mask, simu_mask = self.align_covariate_range(data, simu, covariates)
+        data = data[data_mask]
+        simu = simu[simu_mask]
+
         self.data = data
         self.data_counts = len(data)
         self.simu_counts = len(simu)
@@ -59,6 +67,44 @@ class Inference:
             else:
                 matches = pd.concat([matches, matches_i])
         self.matches = matches
+
+
+    def align_covariate_range(self, data, simu, covariates):
+        """Align range of covariates to avoid artifacts.
+        Returns such a mask for "data" and "simu" respectively: For each element "cov" in "covariates", 
+        that the mask marks the intersection of central specified percentile of the distribution of 
+        data[cov] and simu[cov].
+
+        Args:
+            data (dataframe): Data to select.
+            simu (dataframe): Simulation to refer.
+            covariates (list): A list of strings, corresponding to field names of covariates in matching.
+
+        Returns:
+            data_mask (ndarray, bool):  
+        """
+        central_fraction = self.central_fraction
+
+        assert central_fraction>0 and central_fraction<1, 'central_fraction must be between 0 and 1!'
+        percent = (1-central_fraction)/2
+
+        data_mask = pd.Series([True] * len(data), index=data.index)
+        simu_mask = pd.Series([True] * len(simu), index=simu.index)
+
+        for cov in covariates:
+            data_left_percentile = data[cov].quantile(percent)
+            data_right_percentile = data[cov].quantile(1-percent)
+            simu_left_percentile = simu[cov].quantile(percent)
+            simu_right_percentile = simu[cov].quantile(1-percent)
+
+            intersection_low = max(data_left_percentile, simu_left_percentile)
+            intersection_high = min(data_right_percentile, simu_right_percentile)
+
+            data_mask &= (data[cov] >= intersection_low) & (data[cov] <= intersection_high)
+            simu_mask &= (simu[cov] >= intersection_low) & (simu[cov] <= intersection_high)
+
+        return data_mask, simu_mask
+    
 
     def get_batch_sizes(self, events, batch_size):
         """Optimize the batch sizes based on the rough batch size input.
